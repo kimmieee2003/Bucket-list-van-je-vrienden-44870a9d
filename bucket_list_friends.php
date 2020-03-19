d0ffe2524d58bf17701ecf701f2f8db0c695acde
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-19 14:58:02 +0100

    yeet

--- bucket_list_friends.php
+++ bucket_list_friends.php
@@ -2,16 +2,16 @@
 $aantal = readline("Hoeveel vrienden zal ik vragen om hun droom?");
 if(is_numeric($aantal)) {
     $bucket = array();
-for($i=1; $i <= $aantal; $i++){
+    for($i=1; $i <= $aantal; $i++){
 $naam =readline("wat is je naam?");
 $droom = readline("Wat is jouw droom?");
 $totaal[$naam]=$droom;
 
-}
-foreach($totaal as $naam1 =>$droom1){
+    }
+    foreach($totaal as $naam1 =>$droom1){
 echo "$naam1 heeft als droom $droom1".PHP_EOL;
 
-}
+    }
 }else{
     echo "dit is geen getal.";
 }
